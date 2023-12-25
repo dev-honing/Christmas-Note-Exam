6c8904a8d6615855e5e071cbed2ac86e9fa7af31
Ho Nyeong <dev.honing@gmail.com>
2023-12-26 00:43:57 +0900

    Array.filter() 메서드로 새로운 배열을 생성
    - Array.includes() 메서드를 사용해 '이'가 들어간 포켓몬만 필터링

--- Questoin/Q2.js
+++ Questoin/Q2.js
@@ -11,3 +11,8 @@ const forExam = new ForExam();
 console.log(forExam);
 
 //* Q2. forExam.problem2의 배열에서 이름에 ‘이’가 포함된 포켓몬만 필터링 하시오.
+// Array.filter() 메서드를 사용해 필터링된 새로운 배열을 생성
+forExam.problem2.filter(function (item) {
+  // Array.includes() 메서드를 사용해 '이'가 들어간 포켓몬만 필터링
+  return item.includes('이');
+});
