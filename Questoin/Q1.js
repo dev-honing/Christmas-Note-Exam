31eee77fdf0d75c08c1d91cb0c9966c50bdefe8c
Ho Nyeong <dev.honing@gmail.com>
2023-12-26 00:24:05 +0900

    주석 위치 수정

--- Questoin/Q1.js
+++ Questoin/Q1.js
@@ -3,11 +3,11 @@
 // CommonJS 방식으로 모듈 가져오기
 const ForExam = require('../main');
 
-//* Q1. forExam.problem1의 배열의 모든 요소를 대문자로 변환하시오.
 // new 키워드로 ForExam 클래스의 새로운 인스턴스를 생성하고,
 // forExam 변수에 할당
 const forExam = new ForExam();
 
+//* Q1. forExam.problem1의 배열의 모든 요소를 대문자로 변환하시오.
 // Array.map() 메서드를 사용해서 새로운 배열을 생성하고,
 // Q1에 할당
 const Q1 = forExam.problem1.map(function (item) {
