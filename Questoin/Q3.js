9185cd66648ab5c1fd2b802b8c17f45fca3fcab2
Ho Nyeong <dev.honing@gmail.com>
2023-12-26 00:56:12 +0900

    Q3.js 준비 완료
    - 모듈 가져오기
    - 새로운 인스턴스 생성
    - 디버깅 완료
    - 문제 작성

---
+++ Questoin/Q3.js
@@ -0,0 +1,10 @@
+// Question/Q3.js
+
+// CommonJS 방식으로 모듈 가져오기
+const ForExam = require('../main');
+
+// new 키워드로 ForExam 클래스의 새로운 인스턴스를 생성하고,
+// forExam 변수에 할당
+const forExam = new ForExam();
+
+//* Q3. forExam.problem3의 배열의 모든 요소를 하나의 문자열로 합치시오.
