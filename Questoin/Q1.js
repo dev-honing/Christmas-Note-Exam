9ad0e577fcaaa77a761c99e9800f2f5fcd5d42a0
Ho Nyeong <dev.honing@gmail.com>
2023-12-25 23:56:59 +0900

    Q1에 할당된 새로운 배열을 출력

--- Questoin/Q1.js
+++ Questoin/Q1.js
@@ -8,11 +8,12 @@ const ForExam = require('../main');
 // forExam 변수에 할당
 const forExam = new ForExam();
 
-// 디버깅을 위해 forExam 변수를 콘솔 출력
-console.log(forExam);
-
-// Array.map() 메서드를 사용해서 새로운 배열을 생성
-forExam.problem1.map(function (item) {
+// Array.map() 메서드를 사용해서 새로운 배열을 생성하고,
+// Q1에 할당
+const Q1 = forExam.problem1.map(function (item) {
   // toUpperCase() 메서드를 사용해 각 문자열을 대문자로 변환
   return item.toUpperCase();
 })
+
+// Q1에 할당된 새로운 배열을 출력
+console.log(Q1);
