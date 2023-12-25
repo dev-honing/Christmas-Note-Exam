0a9168adbca7e3dd40b4844e9faf3fbafdc6dbae
Ho Nyeong <dev.honing@gmail.com>
2023-12-26 00:45:24 +0900

    Q2에 할당된 배열을 출력

--- Questoin/Q2.js
+++ Questoin/Q2.js
@@ -7,12 +7,13 @@ const ForExam = require('../main');
 // forExam 변수에 할당
 const forExam = new ForExam();
 
-// 디버깅을 위해 forExam 변수를 콘솔로 출력해 확인
-console.log(forExam);
-
 //* Q2. forExam.problem2의 배열에서 이름에 ‘이’가 포함된 포켓몬만 필터링 하시오.
-// Array.filter() 메서드를 사용해 필터링된 새로운 배열을 생성
-forExam.problem2.filter(function (item) {
+// Array.filter() 메서드를 사용해 필터링된 새로운 배열을 생성하고,
+// Q2에 할당
+const Q2 = forExam.problem2.filter(function (item) {
   // Array.includes() 메서드를 사용해 '이'가 들어간 포켓몬만 필터링
   return item.includes('이');
 });
+
+// Q2에 할당된 필터링된 배열을 출력
+console.log(Q2);
