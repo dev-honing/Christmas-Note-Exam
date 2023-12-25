6caf5101ef8da4b738f8325f888fb881586625a6
Ho Nyeong <dev.honing@gmail.com>
2023-12-25 09:32:28 +0900

    initData() 메서드 정의와 설정 객체 생성
    - initData() 메서드를 추가해 데이터를 초기화
    - 설정 객체를 생성해 배열과 인덱스를 정의

--- main.js
+++ main.js
@@ -9,4 +9,16 @@ class ForExam {
     // initData() 메서드를 호출해 데이터를 초기화
     this.initData();
   }
+
+  // 데이터를 초기화하는 initData() 메서드 정의
+  initData() {
+    // 설정 객체 생성
+    const config = {
+      baseArray: ['피카츄', '라이츄', '꼬부기'],
+      specialProblems: [10, 16, 17, 18, 19, 20],
+      htmlTagProblems: [22, 23, 25, 27, 29, 30],
+      startProblemIndex: 1,
+      endProblemIndex: 30
+    };
+  }
 }
