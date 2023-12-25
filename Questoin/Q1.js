eedc35baf3fd0ac788c29ebc0f55c1a879ffb12e
Ho Nyeong <dev.honing@gmail.com>
2023-12-25 23:29:19 +0900

    편집을 위해 drop

--- Questoin/Q1.js
+++ Questoin/Q1.js
@@ -3,8 +3,6 @@
 // CommonJS 방식으로 모듈 가져오기
 const ForExam = require('../main');
 
-// new 키워드를 사용해 ForExam 인스턴스를 생성하고, 변수 Q1에 할당
-const Q1 = new ForExam();
-
-// 디버깅을 위한 확인 콘솔을 출력
-console.log(Q1);
+//* Q1. forExam.problem1의 배열의 모든 요소를 대문자로 변환하시오.
+// ForExam 클래스의 새로운 인스턴스를 생성하고 forExam 변수에 할당
+const forExam = new ForExam();
