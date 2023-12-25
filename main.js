411ad03abf5329b97687ed48a8a2a5a57b786555
Ho Nyeong <dev.honing@gmail.com>
2023-12-25 09:26:30 +0900

    ForExam 클래스 선언
    - initData() 메서드를 호출해 데이터를 초기화

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 // main.js
 
 // Happy Christmas Note Exam Basic
+
+// ForExam 클래스 선언
+class ForExam {
+  // 생성자
+  constructor() {
+    // initData() 메서드를 호출해 데이터를 초기화
+    this.initData();
+  }
+}
