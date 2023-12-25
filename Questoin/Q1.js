e936d4c0fdc07357de19798c129490e30739e5d0
Ho Nyeong <dev.honing@gmail.com>
2023-12-25 23:53:25 +0900

    Q1.js 준비 완료

--- Questoin/Q1.js
+++ Questoin/Q1.js
@@ -4,5 +4,9 @@
 const ForExam = require('../main');
 
 //* Q1. forExam.problem1의 배열의 모든 요소를 대문자로 변환하시오.
-// ForExam 클래스의 새로운 인스턴스를 생성하고 forExam 변수에 할당
+// new 키워드로 ForExam 클래스의 새로운 인스턴스를 생성하고,
+// forExam 변수에 할당
 const forExam = new ForExam();
+
+// 디버깅을 위해 forExam 변수를 콘솔 출력
+console.log(forExam);
